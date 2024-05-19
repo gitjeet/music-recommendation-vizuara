a27999ff6d51e37f25212a3c849ec67e51dcde4a
Abhijeet Singh <55208353+gitjeet@users.noreply.github.com>
2024-05-19 13:30:29 +0530

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '6dce4f1813mshc87cb80b9ec3143p14b126jsne6f43eebe058');
+      headers.set('X-RapidAPI-Key', '41bd72105fmsh75e3b1bdcc9a403p13dac4jsnccbf73b8b2c7');
 
       return headers;
     },
